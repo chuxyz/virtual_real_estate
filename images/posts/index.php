66a235fd2e23645304aedba8ea5440e693fcc7f4
chuxyz <36097937+chuxyz@users.noreply.github.com>
2022-05-31 15:34:31 +0100

    Add files via upload

---
+++ images/posts/index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+<title>Invalid Request</title>
+</head>
+
+<body>
+<h1>Not Found</h1>
+<p>The requested URL <?php echo $_SERVER['REQUEST_URI']; ?> was not found on this server.</p>
+<p>Additionally, a 404 Not Found error was encountered while trying to use an ErrorDocument to handle the request.</p>
+</body>
+</html>
